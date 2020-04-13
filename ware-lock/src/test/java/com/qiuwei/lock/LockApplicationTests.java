4baeb533b2703ec8bb07ad0706379e6cab7a073f
qiuwei <Wcbc0310@126.com>
2020-04-13 14:38:46 +0800

    初始化分布式锁工程

---
+++ ware-lock/src/test/java/com/qiuwei/lock/LockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qiuwei.lock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
