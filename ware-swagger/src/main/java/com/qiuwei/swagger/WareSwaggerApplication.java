61da60b9044b4587457a839ae821b23a44c9281a
qiuwei <Wcbc0310@126.com>
2020-08-28 21:11:52 +0800

    初始化swagger子项目

---
+++ ware-swagger/src/main/java/com/qiuwei/swagger/WareSwaggerApplication.java
@@ -0,0 +1,17 @@
+package com.qiuwei.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: qiuweib@yonyou.com
+ * @Date: 2020-08-28.
+ */
+@SpringBootApplication
+public class WareSwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WareSwaggerApplication.class, args);
+    }
+
+}
